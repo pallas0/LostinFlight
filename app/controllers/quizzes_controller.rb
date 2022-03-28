3a1274f97a9e0325f66a345d1dab21a59a591c06
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-03-28 14:16:20 -0700

    routes okay so far

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,2 +1,37 @@
 class QuizzesController < ApplicationController
+    def index
+        quizzes = Quiz.all
+        render json: quizzes
+    end
+    def show
+        quiz = find_quiz
+        render json: quiz, status: :ok
+    end
+
+    def create
+        new_entry = Quiz.create!(question_params)
+        render json: new_entry, status: :created
+    end
+
+    def update
+        # byebug
+        to_update = find_question
+        to_update.update(question_params)
+        render json: { "DELETED": to_update }, status: :ok
+    end
+
+    def destroy
+        to_delete = find_question
+        to_delete.delete
+        render json: to_delete, status: :ok
+    end
+    private
+
+    def find_quiz
+        Quiz.find_by!(id: params[:id])
+    end
+
+    def question_params
+        params.permit(:question, :empathy, :assertiveness, :creativity, :ambition, :optimism)
+    end
 end
--- app/models/legend.rb
+++ app/models/legend.rb
@@ -1,2 +1,3 @@
 class Legend < ApplicationRecord
+    has_many :users
 end
