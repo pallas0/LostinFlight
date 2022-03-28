934e0ffb371611e55ff8039c36a542f459c8dbe8
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-03-28 12:19:00 -0700

    new db tables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    def index
+        user = User.all
+        render json: user 
+      end 
+    
+      # This is for session staying logged in
+      def show
+        # byebug
+        user = User.find_by!(id: session[:user_id])
+        if user 
+          render json: user
+        else 
+          render json: {error: "Not authorized"}, status: :unauthorized
+        end 
+      end
 end
