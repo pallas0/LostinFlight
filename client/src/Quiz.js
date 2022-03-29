23abb20ca2e7ac4a398136a0b520991f4535b460
karsyn ansari <karsyn.ansari@gmail.com>
2022-03-28 22:11:28 -0600

    first commit

--- client/src/Quiz.js
+++ client/src/Quiz.js
@@ -6,7 +6,7 @@ function Quiz() {
         <h1>Quiz</h1>
         <h2>Question:</h2>
         <button> Option 1</button>
-        <button> Option 2</button>
+        <button> Option 2 </button>
         <button> Option 3</button>
 
 
